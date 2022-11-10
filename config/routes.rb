885147cd026fbb08044e01202276f524c863c729
stuart wilson <stuartwilson94@gmail.com>
2022-11-09 18:35:45 -0600

    completed members only assignment for TOP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts
+  
+  devise_scope :user do
+    get "users", to: "devise/sessions#new"
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, path: '',
+                path_names: { 
+                sign_in: 'login',
+                sign_out: 'logout',
+                sign_up: 'register'
+                            }
+  root to: 'pages#home'
+
 end
